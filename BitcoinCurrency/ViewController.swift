ce5817ccb3e856892d0126f8fd8513c932c31da7
Felipe Lima <felipe_limacarvalho@hotmail.com>
2021-05-18 16:32:21 -0400

    First Commit

--- BitcoinCurrency/ViewController.swift
+++ BitcoinCurrency/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let publicKey = "ZWVhMzIxYmIwMGQzNDNkZWI2MGYxZDU2MTNhNDgxOTQ"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
